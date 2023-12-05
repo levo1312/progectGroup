25d9a4ed7a199270f2997f81a9368190aa1e5893
levo1312 <luisernestovinasciocoro@gmail.com>
2023-12-05 14:59:22 +0100

    prova lista parola

--- ernesto/IMPICCATO.js
+++ ernesto/IMPICCATO.js
@@ -1,5 +1,6 @@
 //variabili globali
-var parSegreta="ingegni";  //parola prova della logica
+var arrparSegreta=["ingegni", "cocacola", "cane", "fiori", "cioccolato"];  //parola prova della logica
+let parSegreta = arrparSegreta[Math.floor(random()*arrparSegreta.length)];
 let tentativi=[];   //traccia delle lettere inserite 
 let vite=6;//parSegreta.length;   //conteggio vite
 
@@ -7,16 +8,29 @@ let vite=6;//parSegreta.length;   //conteggio vite
 //logica del gioco
 function indovina(lettera) {
     tentativi.push(lettera);  //traccia lettera inserita
-    
+    let mess = '';
+
     let parIndovinata='';   //traccia lettere indovinate
+    let letteEsis = false;
     //ciclo di controllo sulla lettera inserita
     for(let i=0; i<parSegreta.length;i++){
         if(tentativi.includes(parSegreta[i])){
             parIndovinata += parSegreta[i];
+            if(parSegreta[i] === lettera){
+                letteEsis=true;
+            }
         }else{
             parIndovinata+='_';
+            mess = "lettera non presente nella parola";
         }
     }
+
+    // riduci il numero di vite se la lettera non esiste nella parola segreta
+    if (!letteEsis) {
+        vite--;
+    }
+    
+
     
 
     if(vite==0){ //vite esaurite
@@ -32,30 +46,16 @@ function indovina(lettera) {
     document.getElementById("misteryWord").innerHTML = mess;
 }
 
-/*
-da fare:
-    - implementare punteggio
-    - implementare controllo CE
-    
-
-
-//controlla che la lettera in input sia contenuta nella parola
-function controllo() {
-     
-    if(parSegreta.includes(lettera)){
-        mess = "complimenti lettera trovata";
-    }else{
-        vite--;
-        mess = " la lettera scelta non è presente nella parola";
-    }
-}
-*/
-
 
 //gestione input/interazione
 function game(){
     let lettera = document.getElementById("inputUtent").value;
-    
     indovina(lettera);
     document.getElementById("inputUtent").value="";  //pulisce l'input ad ogni tentativo
 }
+
+/*
+da fare:
+    - implementare punteggio
+    - implementare controllo CE
+*/
